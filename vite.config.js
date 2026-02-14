0567fd05cc4c7a16c07aea9c8b7f29695738e569
MarsWangyang <marsmang0525@gmail.com>
2026-02-14 01:41:03 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   // 如果部署到项目页面 (https://username.github.io/repo-name/)
   // 请将 base 设置为 '/repo-name/'
   // 如果部署到用户页面 (https://username.github.io/) 或自定义域名，设置为 '/'
-  base: '/STATUSCODE.ai/',
+  base: '/posture.ai/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
